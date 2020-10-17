0a08558970a0512d476d87dd9ae1c90e7941fee1
yogesh.chaudhari.77 <chaudhari.yogesh20@gmail.com>
2020-10-17 17:58:03 +1100

    Submission Ready Code - Version 5 Employer all functions tested successfully. Employer All Test Cases Verified.

--- src/model/entities/JobCategory.java
+++ src/model/entities/JobCategory.java
@@ -7,8 +7,8 @@ import java.io.Serializable;
 
 public class JobCategory implements Serializable {
 
-	private final String id;
-	private final String categoryTitle;
+	private String id;
+	private String categoryTitle;
 	private static int idNo = 1;
 	private String status;
 	
--- src/model/entities/Qualification.java
+++ src/model/entities/Qualification.java
@@ -10,11 +10,11 @@ import java.util.Date;
 
 public class Qualification implements Serializable {
 
-    private final String qualificationLevel;
-    private final Date startDate;
-    private final Date endDate;
-    private final String fieldOfStudy;
-    private final double marksObtained;
+    private String qualificationLevel;
+    private Date startDate;
+    private Date endDate;
+    private String fieldOfStudy;
+    private double marksObtained;
 
     public Qualification(String qualificationLevel, Date startDate, Date endDate, String fieldOfStudy, double marksObtained) {
         this.qualificationLevel = qualificationLevel;
